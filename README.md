f76d36242b95e7316cfb896472d892f15ea5d493
fuse314 <github@gma.name>
2023-08-04 18:39:06 +0200

    feat: code cleanup, no more shell_exec

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Synology-DDNS-IPv6
-Synology DSM has still very limitted support for DynDNS including IPv6. Except for synology.me, only IPv4 is supported for all the pre-configured providers. As long as the Internet provider assigns static IPv6 addresses, this is not a problem. In Germany and Switzerland, however, many providers are using dynamic IPv6 prefixes for private individuals, which change each time a new connection is established.
+Synology DSM has still very limited support for DynDNS including IPv6. Except for synology.me, only IPv4 is supported for all the pre-configured providers. As long as the Internet provider assigns static IPv6 addresses, this is not a problem. In Germany and Switzerland, however, many providers are using dynamic IPv6 prefixes for private individuals, which change each time a new connection is established.
 
 Your internet router may support DDNS including IPv6, but it will not be a solution to access your Synology over IPv6 from outside. There is a single IPv4 global address (common for the router and e.g. your Synology), but there are different global IPv6 addresses for the router and your Synology. Therefore an DynDNS update routine including IPv6 on the Synology is required to make services on the Synology via IPv6 available from outside and not only your router.
 
@@ -10,19 +10,15 @@ With the following steps you can use a sub domain of your domain hosted at Infom
 2) Copy the php script from this repository to `/usr/syno/bin/ddns/infomaniak46.php`. And make it executable (`chmod 755 /usr/syno/bin/ddns/infomaniak46.php`)
 3) Add to that configuration file `/etc.defaults/ddns_provider.conf` the lines
    ```
-   [INFOMANIAK_4_6]
+   [INFOMANIAK_46]
            modulepath=/usr/syno/bin/ddns/infomaniak46.php
            queryurl=https://infomaniak.com/nic/update
    ```
-4) In the control panel you can now select INFOMANIAK_4_6 from the dropdown, enter your host name (subdomain.yourdomain.com), user name (yourdomain.com) and password.
+4) In the control panel you can now select INFOMANIAK_46 from the dropdown, enter your host name (subdomain.yourdomain.com), username and password (from the setup process at infomaniak.com).
 
-DSM is executing the DDNS update normally once every 24 hours. But sometimes every few minutes and that causes "abuse " response from Infomaniak and a critical DSM Protocoll Center entry. To avoid that, a minimum interval $ageMin_h with preset to 2.0 hours was added.
+DSM is executing the DDNS update normally once every 24 hours. The update request is only sent, if the IP address (ipv4 or ipv6 differs from the current A or AAAA DNS record).
 
 **Important:** In the control panel in the column "External Address" only your IPv4 address will be displayed.
-
-The last line in /tmp/ddns.log contains the response from Infomaniak, e.g.
-
-`curl_exec result: nochg 12.34.56.78 2003:8106:1234:5678:abcd:ef01:2345:6789`
 
 ## Credits and References
 - Changes from Strato to Infomaniak by [fuse314](https://github.com/fuse314/infomaniak-synology-ddns-IPv6)
--- infomaniak46.php
+++ infomaniak46.php
@@ -1,143 +1,162 @@
-#!/usr/bin/php
+#!/usr/bin/php -d open_basedir=/usr/syno/bin/ddns
 <?php
 /*
-Attention: No empty line before this!!!! The output must start with 'good' or 'nochg' in the 1st line for an success message 
-https://community.synology.com/enu/forum/1/post/130109
-https://www.computerbase.de/forum/threads/ddns-mit-ipv6.2057194/
-https://www.programmierer-forum.de/ipv6-ddns-mit-synology-nas-evtl-auf-andere-nas-router-bertragbar-t319393.htm
-https://gist.github.com/hwkr/906685a75af55714a2b696bc37a0830a
-
-Remark: If the command "ip -6 addr" is reporting multiple IPv6 addresses, the 1st global one is used.
-You may change the command and add the name the interface, to e.g. "ip -6 addr list ovs_eth1 ..." 
-The interface names can be extracted from the result of the "ip -6 addr" command output
-Alternative (see below commented out version): Ask Google for the IPv6, whith which the DSM is online
-
-Parameters: 1=account (username, usually domain name), 2=pwd (use single quotes!) 3=hostname (incl. sub domain), 4=ip (IPv4)
-
-This script checks the current A and AAAA records for "hostname" and only sends a request if the addresses have changed.
+ * script by fuse314: https://github.com/fuse314/infomaniak-synology-ddns-IPv6
+ * credit: https://github.com/schmidhorst/synology-ddns-IPv6
+ * Parameters: 1=account (username, usually domain name), 2=pwd (use single quotes) 3=hostname, 4=ip (IPv4)
+ * This script checks the current A and AAAA records for "hostname" and only sends an update request if the IP address(es) have changed.
 */
 
-$date = date('Y-m-d H:i:s');
-$msg = "\n$date Start $argv[0]\n";
+// set this to true for debug output. - execute the script over SSH directly to see the debug output.
+$debug = false;
+
+$msg = '';
+$msg .= $debug ? date('Y-m-d H:i:s') . " Start $argv[0]\n" : '';
+
 if ($argc !== 5) {
-  $msg .= "  Error: Bad param count $argc instead of 5!\n $argv[0] <account> '<PW>' <host> <ipv4>\n";
-  echo $msg;
-  exit("Error: Bad param count $argc instead of 5!");
+  $msg .= "Error: Bad param count $argc instead of 5!\n $argv[0] <account> '<PW>' <host> <ipv4>\n";
+  exit($msg);
 }
-$account = (string)$argv[1]; // Account name
+$account = (string)$argv[1];
 $pwd = (string)$argv[2];
-$hostname = (string)$argv[3]; // sub.domain.com
-$ip = (string)$argv[4];
+$hostname = (string)$argv[3];
+$ipv4 = (string)$argv[4];
 
 // check that the hostname contains '.'
 if (strpos($hostname, '.') === false) {
-  // echo "Error: Badparam hostname $hostname\n";
-  $msg .= "  Error: Badparam hostname $hostname\n";
-  echo "$msg";
-  exit("Error: Badparam hostname $hostname");
+  $msg .= "Error: Badparam hostname must contain a dot .: '$hostname'\n";
+  exit($msg);
 }
 
-if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-  // echo ("Error Badparam: 1st IP is not IPv4 in '$ip'\n");
-  $msg .= "  Error Badparam: 1st IP is not IPv4 in '$ip'\n";
-  echo "$msg";
-  exit("Error Badparam: 1st IP is not IPv4 in '$ip'");
+if (!filter_var($ipv4, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+  $msg .= "Error Badparam: IP is not a valid IPv4 address: '$ipv4'\n";
+  exit($msg);
 }
 
-// Solution without contacting Google:
-// https://forum.feste-ip.net/viewtopic.php?t=468
-// IPV6=$(ip addr list eth0 | grep "global" | cut -d ' ' -f6 | cut -d/ -f1)
-// https://superuser.com/questions/468727/how-to-get-the-ipv6-ip-address-in-linux
-$cmd = "ip -6 addr | grep inet6 | grep 'scope global' | awk -F '[ \t]+|/' '{print $3}' 2>&1";
-// $msg .= "cmd: $cmd\n";
-$ipv6multi = shell_exec($cmd);
-// msg .= "ipv6multi $ipv6multi"; // if more than one LAN active: multiple adresses
-$lines = explode("\n", $ipv6multi);
-$ipv6 = $lines[0];
-$msg .= "  used IPv6: $ipv6\n";
+// use api to get public ipv6 address, choose your preferred api provider...
+//$ipv6 = getIpv6AddressIpify();
+//$ipv6 = getIpv6AddressSeeip();
+$ipv6 = getIpv6AddressGoogle();
+$msg .= $debug ? "Public ipv6 from api: $ipv6\n" : '';
 
-// Alternate Solution: Get the online IPv6 of the disk station:
-// $ipv6 = get_data('https://domains.google.com/checkip');
-
-if (!filter_var($ipv6, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-  echo ("$ipv6 is not a valid IPv6 address\n");
-  $msg .= "  $ipv6 is not a valid IPv6 address\n";
-  $ipv6 = '';
-}
-if (!filter_var($ipv6, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE)) {
-  echo ("$ipv6 is not a valid global IPv6 address\n");
-  $msg .= "  $ipv6 is not a valid global IPv6 address\n";
+if (!filter_var($ipv6, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE)) {
+  $msg .= $debug ? "$ipv6 is not a valid global IPv6 address, ignoring.\n" : '';
   $ipv6 = '';
 }
 
-$dnsipv6 = explode("\n", shell_exec("nslookup -type=aaaa $hostname | /bin/tail -n 4 | /bin/grep \"Address:\" | /bin/cut -d \" \" -f 2"))[0];
-$dnsipv4 = explode("\n", shell_exec("nslookup -type=a $hostname | /bin/tail -n 4 | /bin/grep \"Address:\" | /bin/cut -d \" \" -f 2"))[0];
+$dnsipv4 = getDnsRecord($hostname, true);
+$dnsipv6 = getDnsRecord($hostname, false);
 
-if ($ip == $dnsipv4 && ($ipv6 == '' || $ipv6 == $dnsipv6)) {
-  $msg .= "IP addresses unchanged, no need to update.\n";
-  echo "nochg $ip $ipv6\n";
+$msg .= $debug ? "IPs from dns records: $dnsipv4 / $dnsipv6\n" : '';
+// use inet_pton() to compare ip addresses due to possible shorthand ipv6 addresses (2001::33:ff2a is the same as 2001:0:0:0:0:0:33:ff2a)
+if (inet_pton($ipv4) == inet_pton($dnsipv4) && ($ipv6 == '' || inet_pton($ipv6) == inet_pton($dnsipv6))) {
+  $msg .= $debug ? "IP addresses unchanged, no need to update.\n" : '';
+  echo "nochg $ipv4 $ipv6\n$msg";
   exit;
 }
 
-$msg .= "IP changed: v4: param=$ip DNS=$dnsipv4 v6: param=$ipv6 DNS=$dnsipv6\n";
-$msg .= "preparing request(s)...\n";
+$msg .= $debug ? "IP changed: v4: param=$ipv4 DNS=$dnsipv4 v6: param=$ipv6 DNS=$dnsipv6\n" : '';
+$msg .= $debug ? "Preparing request(s)...\n" : '';
 
 // https://www.infomaniak.com/de/support/faq/2357/dyndns-einrichten-eines-ddns-mit-einer-bei-infomaniak-verwalteten-domain
-// https://username:passwort@infomaniak.com/nic/update?hostname=subdomain.yourdomain.com&myip=12.34.56.78,2000:1200:3400:5600:abcd:ef01:2345:6789  
 
-$url = 'https://infomaniak.com/nic/update?hostname=' . $hostname . '&myip=';
+$url = "https://infomaniak.com/nic/update?hostname=$hostname&myip=";
 
 $result = '';
-if($ip !== $dnsipv4) {
-  $v4url = $url . $ip;
-  $msg .= "  need to update ipv4. Used url: $v4url\n";
+if (inet_pton($ipv4) !== inet_pton($dnsipv4)) {
+  $v4url = $url . $ipv4;
+  $msg .= $debug ? "Update ipv4. Use url: $v4url\n" : '';
   $result = updateHost($v4url, $account, $pwd) . "\n";
 }
-if($ipv6 !== $dnsipv6) {
+if ($ipv6 !== '' && inet_pton($ipv6) !== inet_pton($dnsipv6)) {
   $v6url = $url . $ipv6;
-  $msg .= "  need to update ipv6. Used url: $v6url\n";
-  $result = updateHost($v6url, $account, $pwd);
+  $msg .= $debug ? "Update ipv6. Use url: $v6url\n" : '';
+  $result .= updateHost($v6url, $account, $pwd);
 }
 
-// DEBUG
-//echo $msg;
-
-echo $result; // The script output needs to start(!!) with "nochg" or "good" to avoid error messages in the synology protocol list.
+echo "$result\n$msg";
 if ((strpos($result, "good") !== 0) && (strpos($result, "nochg") !== 0)) {
   syslog(LOG_ERR, "$argv[0]: $result");
 }
 
-function updateHost($fullurl, $user, $pw)
+/*
+ * Init curl request object with common options
+ */
+function initCurl($url)
 {
-  // Send now the actual IPs to the DDNS provider:
   $req = curl_init();
-  curl_setopt($req, CURLOPT_URL, $fullurl);
-  curl_setopt($req, CURLOPT_RETURNTRANSFER, 1); // https://stackoverflow.com/questions/6516902/how-to-get-response-using-curl-in-php
-  curl_setopt($req, CURLOPT_CONNECTTIMEOUT, 25);
-  // without an agent you will get "badagent 0.0.0.0"
-  // https://www.linksysinfo.org/index.php?threads/ddns-custom-url-badagent-error.75520/
+  curl_setopt($req, CURLOPT_URL, $url);
+  curl_setopt($req, CURLOPT_CONNECTTIMEOUT, 10);
+  curl_setopt($req, CURLOPT_TIMEOUT, 30);
+  curl_setopt($req, CURLOPT_VERBOSE, false);
+  curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+  return $req;
+}
+
+/*
+ * Get content from url using ipv6
+ */
+function getContentV6($url)
+{
+  $req = initCurl($url);
+  curl_setopt($req, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V6);
+  $result = curl_exec($req);
+  curl_close($req);
+  return $result;
+}
+
+/*
+ * Get ipv6 address from ipify.org
+ */
+function getIpv6AddressIpify()
+{
+  return getContentV6("https://api64.ipify.org");
+}
+
+/*
+ * Get ipv6 address from google
+ */
+function getIpv6AddressGoogle()
+{
+  return getContentV6("https://domains.google.com/checkip");
+}
+
+/*
+ * Get ipv6 address from seeip
+ */
+function getIpv6AddressSeeip()
+{
+  return getContentV6("https://ipv6.seeip.org");
+}
+
+/*
+ * Send update Request for hostname.
+ * Only one ip address can be updated at a time.
+ */
+function updateHost($url, $user, $pw)
+{
+  $req = initCurl($url);
+  // fake browser user agent
   $agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36';
   curl_setopt($req, CURLOPT_USERAGENT, $agent);
-  // CURLOPT_AUTOREFERER
   curl_setopt($req, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
   curl_setopt($req, CURLOPT_USERPWD, "$user:$pw");
-
-  // $res = 'nochg 9.99.99.99 2003:99:99:99:99:99:99:99'; // may be used for debugging
-  // $res = 'badauth test';
   $res = curl_exec($req);
   curl_close($req);
   return $res;
-  /*
-    https://community.synology.com/enu/forum/17/post/57640, normal responses:
-    good - Update successfully.
-    nochg - Update successfully but the IP address have not changed.
-    nohost - The hostname specified does not exist in this user account.
-    abuse - The hostname specified is blocked for update abuse, too often requisted in short period
-    notfqdn - The hostname specified is not a fully-qualified domain name.
-    badauth - Authenticate failed.
-    911 - There is a problem or scheduled maintenance on provider side
-    badagent - The user agent sent bad request(like HTTP method/parameters is not permitted)
-    badresolv - Failed to connect to because failed to resolve provider address.
-    badconn - Failed to connect to provider because connection timeout.
-  */
+}
+
+/*
+ * Get DNS Record for hostname
+ * Either A (ipv4) or AAAA (ipv6) records are queried.
+ */
+function getDnsRecord($hostname, $v4)
+{
+  $result = dns_get_record($hostname, $v4 ? DNS_A : DNS_AAAA);
+  if ($result !== false) {
+    $prop = $v4 ? "ip" : "ipv6";
+    return $result[0][$prop];
+  } else {
+    return '';
+  }
 }
