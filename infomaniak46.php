516f613d0d156e73f46d3eeecec254daa6d85067
Gottfried Mayer <github@gma.name>
2024-03-25 21:11:46 +0100

    fix: error if AAAA entry is missing

--- infomaniak46.php
+++ infomaniak46.php
@@ -153,7 +153,7 @@ function updateHost($url, $user, $pw)
 function getDnsRecord($hostname, $v4)
 {
   $result = dns_get_record($hostname, $v4 ? DNS_A : DNS_AAAA);
-  if ($result !== false) {
+  if ($result !== false && $result !== null && isset($result[0])) {
     $prop = $v4 ? "ip" : "ipv6";
     return $result[0][$prop];
   } else {
